5fd43da3aed2e74e70dbc7c08088ddf2cd664aaa
toom-kaarel.kiisk@myy.haaga-helia.fi <toom-kaarel.kiisk@myy.haaga-helia.fi>
2025-11-17 15:57:34 +0200

    reads the quote well after making it black and white (binarizing) with cv2 library

--- TesseractTool.py
+++ TesseractTool.py
@@ -1,11 +1,23 @@
 from PIL import Image
-
 import pytesseract
+import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 print("start")
-image = Image.open('test_images/mathquote.jpg')
-grayImage = image.convert('L')
-text = pytesseract.image_to_string(grayImage)
+
+# read img with
+image = cv2.imread('test_images/mathquote.jpg')
+
+# convert into black and white
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+
+# save temporary image for PIL
+cv2.imwrite("temporary.jpg", thresh)
+
+text = pytesseract.image_to_string(Image.open("temporary.jpg"))
+
 print(text)
 print("end")
