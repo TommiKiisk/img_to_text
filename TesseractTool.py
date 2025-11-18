ef0bdc8d015bc91032ce23c1d5a0414799fd0719
toom-kaarel.kiisk@myy.haaga-helia.fi <toom-kaarel.kiisk@myy.haaga-helia.fi>
2025-11-18 13:18:13 +0200

    from pdf to img to string works but not fully accurate

--- TesseractTool.py
+++ TesseractTool.py
@@ -1,10 +1,16 @@
 from PIL import Image
 import pytesseract
 import cv2
+from pdf2image import convert_from_path
 
+poppler_path = r"C:\Users\tommi\Downloads\Release-25.11.0-0\poppler-25.11.0\Library\bin"
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 print("start")
+import sys
+print(sys.executable)
+import subprocess
+print(subprocess.getoutput("pdfinfo -v"))
 
 # read img with
 image = cv2.imread('test_images/mathquote.jpg')
@@ -20,4 +26,19 @@ cv2.imwrite("temporary.jpg", thresh)
 text = pytesseract.image_to_string(Image.open("temporary.jpg"))
 
 print(text)
+
+print("part 2: pdf to text")
+
+# converting pdf into images using pdf2image library
+
+pdf_path = r"D:\Tommi\ohke_tekno\img_to_text\test_images\Catan_rules.pdf"
+
+images = convert_from_path(pdf_path, userpw="",)
+
+# images from pdf to string
+for i, image in enumerate(images):
+    text = pytesseract.image_to_string(image)
+    print(f"Text from page {i + 1}: {text}")
+
+
 print("end")
